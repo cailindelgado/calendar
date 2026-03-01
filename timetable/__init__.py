ba7b8ebc107fa05498d1732f69db2757748641a8
Cailin Delgado <c.delgado@uqconnect.edu.au>
2026-03-02 01:37:46 +1000

    Finalised database stuff

--- timetable/__init__.py
+++ timetable/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-def create_app(test_config=None, config_overrides=None):
+def create_app(test_config=None):
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+
+    # Load the models
+    from .models import db
+    from .models.timetable import Events, Person, Fingerprints
+    db.init_app(app)
+
+    # Create the db tables
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+
+    # Register blueprints
     from .views.routes import api
     app.register_blueprint(api)
 
