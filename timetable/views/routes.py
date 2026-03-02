05943fdef05e7869cb1d24f1de2bc4e417bf342b
Cailin Delgado <c.delgado@uqconnect.edu.au>
2026-03-02 13:03:08 +1000

    factorised update method

--- timetable/views/routes.py
+++ timetable/views/routes.py
@@ -17,7 +17,7 @@ def get_events():
     result = []
     for event in events:
         result.append(event.to_dict())
-    
+
     return jsonify(result)
 
 
@@ -40,9 +40,9 @@ def create_event():
     person = Person.query.filter_by(phone_num=p_num, l_name=last_name).first()
     if person is None:
         person = Person(
-            phone_num = p_num, 
-            f_name = request.json.get('f_name'),
-            l_name = last_name,
+            phone_num=p_num,
+            f_name=request.json.get('f_name'),
+            l_name=last_name,
         )
         if 'm_name' in request.json:
             person.m_name = request.json.get('m_name')
@@ -50,15 +50,16 @@ def create_event():
         db.session.flush()
 
     event = Events(
-        person_id = person.id,
-        time = request.json.get('time'),
+        person_id=person.id,
+        time=request.json.get('time'),
     )
+
     if 'description' in request.json:
         event.description = request.json.get('description')
 
     fingerprint = Fingerprints(
-        person_id = person.id,
-        fingerprint = request.headers.get('X-Fingerprint-ID'),
+        person_id=person.id,
+        fingerprint=request.headers.get('X-Fingerprint-ID'),
     )
 
     db.session.add(event)
@@ -71,7 +72,24 @@ def create_event():
 @api.route('/events/<int:id>', methods=['PUT'])
 def update_event(id):
     # same validation check as delete
-    
+    event = Events.query.get(id)
+
+    usr_fingerprint = Fingerprints.query.filter_by(
+        person_id=event.person_id
+    ).first()
+    if usr_fingerprint is None:
+        return jsonify({'error': 'Fingerprint not found'}), 404
+
+    # get fingerprint from user
+    fingerprint = request.headers.get('X-Fingerprint-ID')
+    if fingerprint is None:
+        return jsonify({'error': 'Missing fingerprint'}), 400
+
+    if usr_fingerprint.fingerprint != fingerprint:
+        return jsonify({'error': 'Unauthorised'}), 403
+
+    # person =
+
     # then update similarly with creating a new event
     pass
 
@@ -82,21 +100,51 @@ def delete_event(id):
     if event is None:
         return jsonify({'error': 'Event not found'}), 404
 
-    usr_fingerprint = Fingerprints.query.filter_by(
-        person_id=event.person_id
-        ).first()
-    if usr_fingerprint is None:
-        return jsonify({'error': 'Fingerprint not found'}), 404
-
-    # get fingerprint from user
     fingerprint = request.headers.get('X-Fingerprint-ID')
     if fingerprint is None:
         return jsonify({'error': 'Missing fingerprint'}), 400
 
+    usr_fingerprint = Fingerprints.query.filter_by(
+        person_id=event.person_id
+    ).first()
+    if usr_fingerprint is None:
+        return jsonify({'error': 'Fingerprint not found'}), 404
+
     if usr_fingerprint.fingerprint != fingerprint:
-        return jsonify({'error': 'Unauthorised'}), 403 
-    
+        return jsonify({'error': 'Unauthorised'}), 403
+
     data = event.to_dict()
     db.session.delete(event)
     db.session.commit()
     return jsonify(data), 200
+
+
+"""
+@api.route('/events/<int:id>', methods=['DELETE'])
+def delete_event(id):
+    event = Events.query.get(id)
+    out = validate(event, request.headers.get('X-Fingerprint-ID'))
+
+    if out[1] != 200:
+        return out
+
+    db.session.delete(event)
+    db.session.commit()
+    return out
+
+
+def validate(event: Events, fingerprint: str):
+    if fingerprint is None:
+        return jsonify({'error': 'Missing fingerprint'}), 400
+
+    usr_fingerprint = Fingerprints.query.filter_by(
+        person_id=event.person_id
+    ).first()
+    if usr_fingerprint is None:
+        return jsonify({'error': 'Fingerprint not found'}), 404
+
+    if usr_fingerprint.fingerprint != fingerprint:
+        return jsonify({'error': 'Unauthorised'}), 403
+
+    return jsonify(event.to_dict()), 200
+"""
