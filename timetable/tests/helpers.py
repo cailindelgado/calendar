685452c7390cb9bcf6651ca9629e4a8073b3b236
Cailin Delgado <c.delgado@uqconnect.edu.au>
2026-07-03 01:20:06 +1000

    Refactored test folder

--- timetable/tests/helpers.py
+++ timetable/tests/helpers.py
@@ -8,8 +8,18 @@ FINGERPRINT = '11111111-1111-1111-1111-111111111111'
 OTHER_FINGERPRINT = '22222222-2222-2222-2222-222222222222'
 
 
-def post_event(client, time='2099-01-01T12:00:00', fingerprint=FINGERPRINT, missionary_type='elders', **overrides):
-    payload = {**PERSON, 'time': time, 'missionary_type': missionary_type, **overrides}
+def create_group(client, missionary_type='elders', ward='Brisbane'):
+    r = client.post(
+        '/calendar/group',
+        json={'ward': ward, 'missionary_type': missionary_type},
+    )
+    return r.get_json()['id']
+
+
+def post_event(client, time='2099-01-01T12:00:00', fingerprint=FINGERPRINT, group_id=None, missionary_type='elders', **overrides):
+    if group_id is None:
+        group_id = create_group(client, missionary_type=missionary_type)
+    payload = {**PERSON, 'time': time, 'group_id': group_id, **overrides}
     return client.post(
         '/calendar/events',
         json=payload,
